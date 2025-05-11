6951f3352a9676f210daddb66c1d513c1c0b0e3e
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-05-11 06:48:42 -0500

    feat: add boundaries domain graph names

---
+++ app/src/main/java/com/campusmov/uniride/presentation/navigation/Graph.kt
@@ -0,0 +1,7 @@
+package com.campusmov.uniride.presentation.navigation
+
+object Graph {
+    const val ROOT = "root_graph"
+    const val AUTH = "auth_graph"
+    const val PROFILE = "profile_graph"
+}
