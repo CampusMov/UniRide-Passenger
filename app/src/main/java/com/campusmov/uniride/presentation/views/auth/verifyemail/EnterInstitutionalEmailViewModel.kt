d1dbcdebd43a553486b423d1bc7fad92585ca307
Eric396 <u20221c857@upc.edu.pe>
2025-05-13 14:14:23 -0500

    feat: add state and view model for enter institutional email
    Add functionality to verify that the email is a university email address

---
+++ app/src/main/java/com/campusmov/uniride/presentation/views/auth/verifyemail/EnterInstitutionalEmailViewModel.kt
@@ -0,0 +1,47 @@
+package com.campusmov.uniride.presentation.views.auth.verifyemail
+
+import android.util.Log
+import android.util.Patterns
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.campusmov.uniride.domain.auth.model.AuthEmailVerificationResponse
+import com.campusmov.uniride.domain.auth.usecases.AuthUseCase
+import com.campusmov.uniride.domain.shared.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class EnterInstitutionalEmailViewModel @Inject constructor(
+    private val authUseCase: AuthUseCase
+):ViewModel() {
+
+    var state = mutableStateOf(InstitutionalEmailValidationState())
+        private set
+
+    var errorMessage = mutableStateOf("")
+        private set
+
+    var verifyEmailResponse = mutableStateOf<Resource<AuthEmailVerificationResponse>?>(null)
+        private set
+
+    fun onEmailInput(email: String) {
+        state.value = state.value.copy(email = email)
+    }
+
+    fun isEmailValid(): Boolean {
+        val emailPattern = Patterns.EMAIL_ADDRESS.matcher(state.value.email).matches()
+        val universityDomainPattern = Regex("[a-zA-Z0-9]+\\.edu\\.pe$").matches(state.value.email.substringAfter("@"))
+        return emailPattern && universityDomainPattern
+    }
+
+    fun sendVerificationEmail() = viewModelScope.launch {
+        if (isEmailValid()) {
+            val result = authUseCase.verifyEmail(state.value.email)
+        } else {
+            Log.d("TAG", "Invalid email format ${state.value.email}")
+            // Handle invalid email case
+        }
+    }
+}
