5d3b992db17e62c6058610df95a84ef418de57fe
Eric396 <u20221c857@upc.edu.pe>
2025-05-13 14:13:05 -0500

    feat: set configuration for dependency inyection with dagger retrofit
    Set for network, repositories and use cases

---
+++ app/src/main/java/com/campusmov/uniride/di/NetworkModule.kt
@@ -0,0 +1,41 @@
+package com.campusmov.uniride.di
+
+import com.campusmov.uniride.core.Config
+import com.campusmov.uniride.data.datasource.remote.service.AuthService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl(Config.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthService(retrofit: Retrofit): AuthService {
+        return  retrofit.create(AuthService::class.java)
+    }
+}
---
+++ app/src/main/java/com/campusmov/uniride/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.campusmov.uniride.di
+
+import com.campusmov.uniride.data.datasource.remote.service.AuthService
+import com.campusmov.uniride.data.repository.auth.AuthRepositoryImpl
+import com.campusmov.uniride.domain.auth.repository.AuthRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    fun provideAuthRepository(authService: AuthService): AuthRepository = AuthRepositoryImpl(authService)
+}
