8bb1197be095df016817c01eba97c979440eebf7
Eric <erich56000@gmail.com>
2025-05-14 01:24:20 -0500

    feat: add verify code service and response class and fix verification email service response

--- app/src/main/java/com/campusmov/uniride/data/datasource/remote/service/AuthService.kt
+++ app/src/main/java/com/campusmov/uniride/data/datasource/remote/service/AuthService.kt
@@ -1,6 +1,6 @@
 package com.campusmov.uniride.data.datasource.remote.service
 
-import com.campusmov.uniride.domain.auth.model.AuthEmailVerificationResponse
+import com.campusmov.uniride.domain.auth.model.AuthVerificationCodeResponse
 import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -11,5 +11,14 @@ interface AuthService {
     @POST("api/v1/registration/sign-up")
     suspend fun sendVerificationEmail(
         @Field("email") email: String,
-    ): Response<AuthEmailVerificationResponse>
+    ): Response<Unit>
+
+    @FormUrlEncoded
+    @POST("api/v1/login/verify-account")
+    suspend fun sendVerificationCode(
+        @Field("email") email: String,
+        @Field("verificationCode") verificationCode: String,
+        @Field("role") role: String
+    ): Response<AuthVerificationCodeResponse>
+
 }
