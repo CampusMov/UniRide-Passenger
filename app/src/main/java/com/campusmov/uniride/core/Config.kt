3124440dff59749b79c92c4fd1aab3c1cefab24b
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-06-08 21:14:53 -0500

    chore: update base url

--- app/src/main/java/com/campusmov/uniride/core/Config.kt
+++ app/src/main/java/com/campusmov/uniride/core/Config.kt
@@ -1,6 +1,5 @@
 package com.campusmov.uniride.core
 
 object Config {
-    const val BASE_URL_AUTH = "http://34.59.220.57/"
-    const val BASE_URL_PROFILE = "http://34.133.90.92/"
+    const val SERVICES_BASE_URL = "http://10.0.2.2:8765/"
 }
