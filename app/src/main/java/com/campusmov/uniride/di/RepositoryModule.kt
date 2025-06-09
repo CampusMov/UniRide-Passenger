45b003c59465c5d236d60deedfec9cf7e64357ae
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-06-08 21:16:37 -0500

    feat(profile): integrates profile service and repository
    - Introduces ProfileService in NetworkModule for handling profile-related API calls.
    - Implements ProfileRepository and its interface for managing profile data.
    - Adds ProfileUseCases for business logic, specifically SaveProfileUseCase.
    - Refactors base URLs to use Config.SERVICES_BASE_URL, consolidating service endpoints.

--- app/src/main/java/com/campusmov/uniride/di/RepositoryModule.kt
+++ app/src/main/java/com/campusmov/uniride/di/RepositoryModule.kt
@@ -3,10 +3,13 @@ package com.campusmov.uniride.di
 import com.campusmov.uniride.data.datasource.local.datastore.LocalDataStore
 import com.campusmov.uniride.data.datasource.location.LocationDataSource
 import com.campusmov.uniride.data.datasource.remote.service.AuthService
+import com.campusmov.uniride.data.datasource.remote.service.ProfileService
 import com.campusmov.uniride.data.repository.auth.AuthRepositoryImpl
 import com.campusmov.uniride.data.repository.location.LocationRepositoryImpl
+import com.campusmov.uniride.data.repository.profile.ProfileRepositoryImpl
 import com.campusmov.uniride.domain.auth.repository.AuthRepository
 import com.campusmov.uniride.domain.location.repository.LocationRepository
+import com.campusmov.uniride.domain.profile.repository.ProfileRepository
 import com.google.android.libraries.places.api.net.PlacesClient
 import dagger.Module
 import dagger.Provides
@@ -22,4 +25,7 @@ object RepositoryModule {
 
     @Provides
     fun provideLocationRepository(locationDataSource: LocationDataSource, placesClient: PlacesClient): LocationRepository = LocationRepositoryImpl(locationDataSource, placesClient)
+
+    @Provides
+    fun provideProfileRepository(profileService: ProfileService): ProfileRepository = ProfileRepositoryImpl(profileService)
 }
