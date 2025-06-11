17de4dd8afa9a68229c1b04a43daca1a0c53040f
Brayan Smith Morales <102981505+Brays83@users.noreply.github.com>
2025-06-10 22:52:28 -0500

    fix: fixed navigation between components

--- app/src/main/java/com/campusmov/uniride/presentation/navigation/graph/root/RootNavGraph.kt
+++ app/src/main/java/com/campusmov/uniride/presentation/navigation/graph/root/RootNavGraph.kt
@@ -6,15 +6,18 @@ import androidx.navigation.compose.NavHost
 import com.campusmov.uniride.presentation.navigation.Graph
 import com.campusmov.uniride.presentation.navigation.graph.auth.AuthNavGraph
 import com.campusmov.uniride.presentation.navigation.graph.profile.ProfileNavGraph
+import com.campusmov.uniride.presentation.navigation.graph.routingmatching.RoutingMatchingNavGraph
 
 @Composable
 fun RootNavGraph(navHostController: NavHostController) {
+
     NavHost(
         navController = navHostController,
         route = Graph.ROOT,
         startDestination = Graph.AUTH
     ) {
         AuthNavGraph(navHostController)
+        RoutingMatchingNavGraph(navHostController)
         ProfileNavGraph(navHostController)
     }
 }
--- app/src/main/java/com/campusmov/uniride/presentation/navigation/graph/routingmatching/RoutingMatchingNavGraph.kt
+++ app/src/main/java/com/campusmov/uniride/presentation/navigation/graph/routingmatching/RoutingMatchingNavGraph.kt
@@ -1,15 +1,30 @@
 package com.campusmov.uniride.presentation.navigation.graph.routingmatching
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 import com.campusmov.uniride.presentation.navigation.Graph
 import com.campusmov.uniride.presentation.navigation.screen.routingmatching.RoutingMatchingScreen
 import com.campusmov.uniride.presentation.views.routingmatching.mapcontent.MapCarpoolSearcherView
 
-@Composable
-fun RoutingMatchingNavGraph(navHostController: NavHostController) {
+//@Composable
+fun NavGraphBuilder.RoutingMatchingNavGraph(navHostController: NavHostController) {
+    navigation(
+        route = Graph.HOME,
+        startDestination = RoutingMatchingScreen.SearchCarpool.route
+    ){
+        composable(route = RoutingMatchingScreen.SearchCarpool.route) {
+            MapCarpoolSearcherView(navHostController = navHostController)
+        }
+        composable(route = RoutingMatchingScreen.AddPickUpPoint.route) {
+            // TODO("Implement the Carpool Details Screen")
+        }
+    }
+
+    /*
     NavHost(
         navController = navHostController,
         route = Graph.HOME,
@@ -21,5 +36,5 @@ fun RoutingMatchingNavGraph(navHostController: NavHostController) {
         composable(route = RoutingMatchingScreen.AddPickUpPoint.route) {
             // TODO("Implement the Carpool Details Screen")
         }
-    }
+    }*/
 }
