4689228f010f3625c10368ac6dc7e4653e2f0935
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2025-06-11 19:57:55 -0500

    feat(auth): implement email verification Flow
    - Enhances user authentication by implementing an email verification flow
    - Adds loading state and button availability management to improve user experience
    - Saves user locally upon successful email verification for future use
    - Improves error handling and UI feedback during the verification process

--- app/src/main/java/com/campusmov/uniride/presentation/views/auth/verifyemail/EnterInstitutionalEmailViewModel.kt
+++ app/src/main/java/com/campusmov/uniride/presentation/views/auth/verifyemail/EnterInstitutionalEmailViewModel.kt
@@ -10,6 +10,7 @@ import com.campusmov.uniride.domain.auth.usecases.AuthUseCase
 import com.campusmov.uniride.domain.auth.usecases.UserUseCase
 import com.campusmov.uniride.domain.shared.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,40 +23,62 @@ class EnterInstitutionalEmailViewModel @Inject constructor(
     var state = mutableStateOf(InstitutionalEmailValidationState())
         private set
 
-    var errorMessage = mutableStateOf("")
-        private set
-
     var verifyEmailResponse = mutableStateOf<Resource<Unit>?>(null)
         private set
 
+    private val _isLoading  = MutableStateFlow(false)
+    val isLoading: MutableStateFlow<Boolean> get() = _isLoading
+
+    private val _isButtonAvailable = MutableStateFlow(false)
+    val isButtonAvailable: MutableStateFlow<Boolean> get() = _isButtonAvailable
+
     fun onEmailInput(email: String) {
         state.value = state.value.copy(email = email.trim())
+        _isButtonAvailable.value = isEmailValid()
     }
 
-    fun isEmailValid(): Boolean {
+    private fun isEmailValid(): Boolean {
         val emailPattern = Patterns.EMAIL_ADDRESS.matcher(state.value.email).matches()
         val universityDomainPattern = Regex("[a-zA-Z0-9]+\\.edu\\.pe$").matches(state.value.email.substringAfter("@"))
         return emailPattern && universityDomainPattern
     }
 
-    fun sendVerificationEmail() = viewModelScope.launch {
-        if (isEmailValid()) {
-            errorMessage.value = ""
+    fun sendVerificationEmail() {
+        viewModelScope.launch {
             deleteAllUsersLocally()
+            _isLoading.value = true
+            _isButtonAvailable.value = false
             val result = authUseCase.verifyEmail(state.value.email)
-            if (result is Resource.Success) {
-                Log.d("TAG", "Email verification successful: ${state.value.email}")
-                verifyEmailResponse.value = result
-                val user = User(email = state.value.email)
-                userUseCase.saveUserLocallyUseCase(user)
+            when(result) {
+                is Resource.Failure -> {
+                    Log.e("TAG", "Error sending verification email: ${result.message}")
+                    _isLoading.value = false
+                    _isButtonAvailable.value = true
+                    state.value.email = ""
+                }
+                is Resource.Success -> {
+                    Log.d("TAG", "Verification email sent successfully")
+                    deleteAllUsersLocally()
+                    verifyEmailResponse.value = result
+                    saveUserLocally(state.value.email)
+                    _isLoading.value = false
+                    _isButtonAvailable.value = false
+                    state.value.email = ""
+                }
+                is Resource.Loading -> {}
             }
-        } else {
-            errorMessage.value = "Formato de correo inválido"
-            Log.d("TAG", "Invalid email format ${state.value.email}")
         }
     }
 
-    fun deleteAllUsersLocally() {
+    private fun saveUserLocally(email: String) {
+        val user = User(email = email)
+        viewModelScope.launch {
+            userUseCase.saveUserLocallyUseCase(user)
+            Log.d("TAG", "User saved locally: ${user.email}")
+        }
+    }
+
+    private fun deleteAllUsersLocally() {
         viewModelScope.launch {
             userUseCase.deleteAllUsersLocallyUseCase()
             Log.d("TAG", "All users deleted locally")
