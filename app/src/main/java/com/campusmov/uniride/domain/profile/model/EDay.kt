732784a7c4f563979701254c8e9015eb2ff50046
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-06-15 22:23:05 -0500

    chore: add eday enum

---
+++ app/src/main/java/com/campusmov/uniride/domain/profile/model/EDay.kt
@@ -0,0 +1,37 @@
+package com.campusmov.uniride.domain.profile.model
+
+enum class EDay {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+
+    companion object {
+        fun fromString(day: String?): EDay {
+            return when (day?.trim()?.uppercase()) {
+                "MONDAY" -> MONDAY
+                "TUESDAY" -> TUESDAY
+                "WEDNESDAY" -> WEDNESDAY
+                "THURSDAY" -> THURSDAY
+                "FRIDAY" -> FRIDAY
+                "SATURDAY" -> SATURDAY
+                "SUNDAY" -> SUNDAY
+                else -> MONDAY
+            }
+        }
+    }
+    fun showDay(): String {
+        return when (this) {
+            MONDAY -> "Lunes"
+            TUESDAY -> "Martes"
+            WEDNESDAY -> "Miercoles"
+            THURSDAY -> "Jueves"
+            FRIDAY -> "Viernes"
+            SATURDAY -> "Jueves"
+            SUNDAY -> "Domingo"
+        }
+    }
+}
