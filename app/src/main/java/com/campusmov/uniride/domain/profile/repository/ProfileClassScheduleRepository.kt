c6daee1b76e5555a2bbfedabc1ebe515449b0b28
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-06-15 22:26:14 -0500

    feat: add ProfileClassScheduleRepository
    Add getClassScheduleByProfileId function

---
+++ app/src/main/java/com/campusmov/uniride/domain/profile/repository/ProfileClassScheduleRepository.kt
@@ -0,0 +1,8 @@
+package com.campusmov.uniride.domain.profile.repository
+
+import com.campusmov.uniride.domain.profile.model.ClassSchedule
+import com.campusmov.uniride.domain.shared.util.Resource
+
+interface ProfileClassScheduleRepository {
+    suspend fun getClassScheduleByProfileId(profileId: String): Resource<List<ClassSchedule>>
+}
