6006e2ae756c0bdd2f57be05e8cf9f8b4a2e7842
Eric <erich56000@gmail.com>
2025-06-17 11:21:11 -0500

    feat(student-metrics): add student rating metrics view model

---
+++ app/src/main/java/com/campusmov/uniride/presentation/views/analytics/studentRating/StudentRatingMetricsViewModel.kt
@@ -0,0 +1,78 @@
+package com.campusmov.uniride.presentation.views.analytics.studentRating
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.campusmov.uniride.domain.analytics.usecases.AnalyticsUseCase
+import com.campusmov.uniride.domain.auth.model.User
+import com.campusmov.uniride.domain.auth.usecases.UserUseCase
+import com.campusmov.uniride.domain.shared.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class StudentRatingMetricsViewModel @Inject constructor(
+    private val analyticsUseCase: AnalyticsUseCase,
+    private val userUseCase: UserUseCase
+): ViewModel(){
+
+    private val _rating =  MutableStateFlow(0.0)
+    val rating : StateFlow<Double> get() = _rating
+
+    private val _user = MutableStateFlow<User?>(null)
+    val user: StateFlow<User?> get() = _user
+
+
+    init {
+        getUserLocally()
+    }
+
+    private fun getUserLocally() {
+        viewModelScope.launch {
+            val result = userUseCase.getUserLocallyUseCase()
+            when (result) {
+                is Resource.Success -> {
+                    _user.value = result.data
+                    Log.d("TAG", "getUser: ${result.data}")
+                    getStudentAverageRating()
+                }
+                is Resource.Failure -> {}
+                Resource.Loading -> {}
+            }
+        }
+    }
+
+    fun getStudentAverageRating() {
+        viewModelScope.launch {
+            try {
+                val userId = _user.value?.id
+                userId?.let {
+                    val result = analyticsUseCase.getStudentRatingMetrics(it)
+                    when(result){
+                        is Resource.Success -> {
+                            _rating.value = result.data.averageRating ?: 0.0
+                            Log.d("TAG", "Rating obtenida: ${_rating.value}")
+                        }
+                        is Resource.Failure -> {
+                            Log.e("StudentRatingMetricsVM", "Error obteniendo rating: ${result.message}")
+                        }
+                        Resource.Loading -> {
+                            Log.d("StudentRatingMetricsVM", "Cargando rating...")
+                        }
+                    }
+
+
+                }
+            } catch (e: Exception) {
+                Log.e("StudentRatingMetricsVM", "Error obteniendo rating: ${e.message}")
+            }
+        }
+    }
+
+
+
+
+}
