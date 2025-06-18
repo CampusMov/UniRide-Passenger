537d112143dc3f1786d16ce15eacf1f765240186
Eric <erich56000@gmail.com>
2025-06-17 20:10:58 -0500

    feat(student-metrics): implement function to get profile by id in repository

--- app/src/main/java/com/campusmov/uniride/domain/profile/repository/ProfileRepository.kt
+++ app/src/main/java/com/campusmov/uniride/domain/profile/repository/ProfileRepository.kt
@@ -5,4 +5,5 @@ import com.campusmov.uniride.domain.shared.util.Resource
 
 interface ProfileRepository {
     suspend fun saveProfile(profile: Profile): Resource<Unit>
+    suspend fun getProfileById(profileId: String): Resource<Profile>
 }
