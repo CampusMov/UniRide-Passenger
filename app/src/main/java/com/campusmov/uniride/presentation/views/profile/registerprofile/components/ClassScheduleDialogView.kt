695e6dfd0d882047f2d629bb63431b20bd8d1fbc
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-06-17 21:13:05 -0500

    feat(schedule): improves schedule dialog UI
    - Improves the UI of the class schedule dialog for better user experience.
    - Adjusts the layout, spacing, and animations for a more visually appealing and functional design.
    - Fixes the visibility of the schedule dialog.

--- app/src/main/java/com/campusmov/uniride/presentation/views/profile/registerprofile/components/ClassScheduleDialogView.kt
+++ app/src/main/java/com/campusmov/uniride/presentation/views/profile/registerprofile/components/ClassScheduleDialogView.kt
@@ -3,7 +3,6 @@ package com.campusmov.uniride.presentation.views.profile.registerprofile.compone
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
@@ -14,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -75,177 +75,201 @@ fun ClassScheduleDialogView(
             dismissOnClickOutside = true
         )
     ) {
-        AnimatedVisibility(
-            visible = true,
-            enter = slideInVertically(initialOffsetY = { it }, animationSpec = tween(300)),
-            exit = slideOutVertically(targetOffsetY = { it }, animationSpec = tween(300))
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter
         ) {
-            Column(
-                Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(fraction = 0.9f)
-                    .background(Color.Black, RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.SpaceBetween,
+            AnimatedVisibility(
+                visible = true,
+                enter = slideInVertically { fullHeight -> fullHeight },
+                exit  = slideOutVertically { fullHeight -> fullHeight }
             ) {
-                Row(
+                Column(
                     Modifier
                         .fillMaxWidth()
-                        .padding(top = 30.dp, bottom = 21.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Spacer(
-                        modifier = Modifier
-                            .size(50.dp)
-                            .background(Color.Transparent)
-
-                    )
-                    Text(
-                        text = if (currentClassScheduleState.isEditing) "Actualizar horario" else "Agregar horario",
-                        fontSize = 30.sp,
-                        fontWeight = FontWeight.ExtraBold,
-                        color = Color.White,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth(0.7f)
-                            .padding(start = 16.dp),
-                        lineHeight = 40.sp
-                    )
-                    IconButton(
-                        modifier = Modifier
-                            .padding(start = 16.dp)
-                            .clip(CircleShape)
-                            .background(Color(0xFF3F4042)),
-                        onClick = {
-                            viewModel.onCloseScheduleDialog()
-                        }
-                    ) {
-                        Icon(
-                            modifier = Modifier
-                                .size(40.dp),
-                            imageVector = Icons.Rounded.Close,
-                            contentDescription = "Close",
-                            tint = Color.White
+                        .fillMaxHeight(fraction = 1f)
+                        .background(
+                            Color.Black,
+                            RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
                         )
-                    }
-                }
-
-                Text("Nombre del curso", fontSize = 16.sp, color = Color.White)
-                DefaultRoundedInputField(
-                    value = currentClassScheduleState.courseName,
-                    onValueChange = { viewModel.onScheduleCourseNameInput(it) },
-                    placeholder = "Matemática básica"
-                )
-
-                Text("Hora de inicio", fontSize = 16.sp, color = Color.White)
-
-                TimePickerInputField(
-                    time = currentClassScheduleState.startedAt,
-                    onTimeChange = { lt -> viewModel.onScheduleStartTimeInput(lt) }
-                )
-
-                Text("Hora de salida", fontSize = 16.sp, color = Color.White)
-
-                TimePickerInputField(
-                    time = currentClassScheduleState.endedAt,
-                    onTimeChange = { lt -> viewModel.onScheduleEndTimeInput(lt) }
-                )
-                Text("Día de la semana", fontSize = 16.sp, color = Color.White)
-                Row(
-                    Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.SpaceBetween,
                 ) {
-                    listOf(
-                        EDay.MONDAY, EDay.TUESDAY, EDay.WEDNESDAY,
-                        EDay.THURSDAY, EDay.FRIDAY, EDay.SATURDAY, EDay.SUNDAY
-                    ).forEach { day ->
-                        val sel = currentClassScheduleState.selectedDay == day
-                        Box(
-                            Modifier
-                                .size(36.dp)
-                                .background(if (sel) Color(0xFF3F4042) else Color.Transparent, CircleShape)
-                                .clickable { viewModel.onScheduleDaySelected(day) },
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(day.name.first().toString(), fontSize = 14.sp, color = Color.White)
-                        }
-                    }
-                }
-
-                Text("Universidad ubicación", fontSize = 16.sp, color = Color.White)
-                DefaultRoundedInputField(
-                    value = locRaw.value,
-                    onValueChange = { location ->
-                        locRaw.value = location
-                        if (location.isBlank()) {
-                            viewModel.onScheduleLocationCleared()
-                        } else {
-                            viewModel.onScheduleLocationQueryChange(location)
-                        }
-                    },
-                    placeholder = "Surco, Primavera 2653",
-                    enableLeadingIcon = true,
-                )
-
-                AnimatedVisibility(visible = currentClassScheduleState.selectedLocation == null) {
-                    LazyColumn(
+                    Row(
                         Modifier
                             .fillMaxWidth()
-                            .height(120.dp)
-                            .padding(top = 16.dp)
+                            .padding(top = 30.dp, bottom = 21.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
                     ) {
-                        itemsIndexed(locationPredictions) { index, prediction ->
-                            Row(
-                                Modifier
-                                    .fillMaxWidth()
-                                    .clickable {
-                                        viewModel.onScheduleLocationSelected(prediction)
-                                        locRaw.value = prediction.fullText
-                                    }
-                                    .padding(vertical = 12.dp),
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Text(prediction.fullText, fontSize = 14.sp, color = Color.White)
+                        Spacer(
+                            modifier = Modifier
+                                .size(40.dp)
+                                .background(Color.Transparent)
+
+                        )
+                        Text(
+                            text = if (currentClassScheduleState.isEditing) "Actualizar horario" else "Agregar horario",
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth(0.7f)
+                                .padding(start = 10.dp),
+                            lineHeight = 40.sp
+                        )
+                        IconButton(
+                            modifier = Modifier
+                                .padding(start = 10.dp)
+                                .clip(CircleShape)
+                                .background(Color(0xFF3F4042)),
+                            onClick = {
+                                viewModel.onCloseScheduleDialog()
                             }
-                            if (index < locationPredictions.lastIndex) {
-                                HorizontalDivider(
-                                    thickness = 1.dp,
-                                    color = Color.White,
-                                    modifier  = Modifier.padding(horizontal = 16.dp)
+                        ) {
+                            Icon(
+                                modifier = Modifier
+                                    .size(40.dp),
+                                imageVector = Icons.Rounded.Close,
+                                contentDescription = "Close",
+                                tint = Color.White
+                            )
+                        }
+                    }
+
+                    Text("Nombre del curso", fontSize = 16.sp, color = Color.White)
+                    DefaultRoundedInputField(
+                        value = currentClassScheduleState.courseName,
+                        onValueChange = { viewModel.onScheduleCourseNameInput(it) },
+                        placeholder = "Matemática básica"
+                    )
+
+                    Text("Hora de inicio", fontSize = 16.sp, color = Color.White)
+
+                    TimePickerInputField(
+                        time = currentClassScheduleState.startedAt,
+                        onTimeChange = { lt -> viewModel.onScheduleStartTimeInput(lt) }
+                    )
+
+                    Text("Hora de salida", fontSize = 16.sp, color = Color.White)
+
+                    TimePickerInputField(
+                        time = currentClassScheduleState.endedAt,
+                        onTimeChange = { lt -> viewModel.onScheduleEndTimeInput(lt) }
+                    )
+                    Text("Día de la semana", fontSize = 16.sp, color = Color.White)
+                    Row(
+                        Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        listOf(
+                            EDay.MONDAY, EDay.TUESDAY, EDay.WEDNESDAY,
+                            EDay.THURSDAY, EDay.FRIDAY, EDay.SATURDAY, EDay.SUNDAY
+                        ).forEach { day ->
+                            val sel = currentClassScheduleState.selectedDay == day
+                            Box(
+                                Modifier
+                                    .size(36.dp)
+                                    .background(
+                                        if (sel) Color(0xFF3F4042) else Color.Transparent,
+                                        CircleShape
+                                    )
+                                    .clickable { viewModel.onScheduleDaySelected(day) },
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(
+                                    day.showDay().first().toString(),
+                                    fontSize = 14.sp,
+                                    color = Color.White
                                 )
                             }
                         }
                     }
-                }
-                if (isCurrentClassScheduleValid) {
-                    if(currentClassScheduleState.isEditing){
-                        DefaultRoundedTextButton(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = "Actualizar",
-                            onClick  = { viewModel.editExistingClassSchedule() }
-                        )
-                        Text(
-                            modifier = Modifier
-                                .padding(top = 8.dp)
+
+                    Text("Universidad ubicación", fontSize = 16.sp, color = Color.White)
+                    DefaultRoundedInputField(
+                        value = locRaw.value,
+                        onValueChange = { location ->
+                            locRaw.value = location
+                            if (location.isBlank()) {
+                                viewModel.onScheduleLocationCleared()
+                            } else {
+                                viewModel.onScheduleLocationQueryChange(location)
+                            }
+                        },
+                        placeholder = "Surco, Primavera 2653",
+                        enableLeadingIcon = true,
+                    )
+
+                    AnimatedVisibility(visible = currentClassScheduleState.selectedLocation == null) {
+                        LazyColumn(
+                            Modifier
                                 .fillMaxWidth()
-                                .clickable {
-                                    viewModel.onDeleteSchedule()
-                                },
-                            text = "Eliminar horario",
-                            fontSize = 12.sp,
-                            style = TextStyle(
-                                textDecoration = TextDecoration.Underline
-                            ),
-                            color = Color.White,
-                            textAlign = TextAlign.Center
-                        )
-                    } else {
-                        DefaultRoundedTextButton(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = "Agregar",
-                            onClick  = { viewModel.addClasScheduleToProfile() }
-                        )
+                                .height(120.dp)
+                                .padding(top = 16.dp)
+                        ) {
+                            itemsIndexed(locationPredictions) { index, prediction ->
+                                Row(
+                                    Modifier
+                                        .fillMaxWidth()
+                                        .clickable {
+                                            viewModel.onScheduleLocationSelected(prediction)
+                                            locRaw.value = prediction.fullText
+                                        }
+                                        .padding(vertical = 12.dp),
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text(prediction.fullText, fontSize = 14.sp, color = Color.White)
+                                }
+                                if (index < locationPredictions.lastIndex) {
+                                    HorizontalDivider(
+                                        thickness = 1.dp,
+                                        color = Color.White,
+                                        modifier = Modifier.padding(horizontal = 16.dp)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    if (isCurrentClassScheduleValid) {
+                        if (currentClassScheduleState.isEditing) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                DefaultRoundedTextButton(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    text = "Actualizar",
+                                    onClick = { viewModel.editExistingClassSchedule() }
+                                )
+                                Text(
+                                    modifier = Modifier
+                                        .padding(top = 15.dp, bottom = 10.dp)
+                                        .fillMaxWidth()
+                                        .clickable {
+                                            viewModel.onDeleteSchedule()
+                                        },
+                                    text = "Eliminar horario",
+                                    style = TextStyle(
+                                        textDecoration = TextDecoration.Underline,
+                                        color = Color.White,
+                                        textAlign = TextAlign.Center,
+                                        fontSize = 12.sp,
+                                        lineHeight = 10.sp
+                                    ),
+                                )
+                            }
+                        } else {
+                            DefaultRoundedTextButton(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(bottom = 10.dp),
+                                text = "Agregar",
+                                onClick = { viewModel.addClasScheduleToProfile() }
+                            )
+                        }
                     }
                 }
             }
