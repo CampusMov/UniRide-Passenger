cb43bbf90213159c12fd414938d222215ccb8c9a
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-06-19 04:47:08 -0500

    feat: add  getAllPassengerRequestsByPassengerId in PassengerRequestService

--- app/src/main/java/com/campusmov/uniride/data/datasource/remote/service/PassengerRequestService.kt
+++ app/src/main/java/com/campusmov/uniride/data/datasource/remote/service/PassengerRequestService.kt
@@ -1,12 +1,20 @@
 package com.campusmov.uniride.data.datasource.remote.service
 
 import com.campusmov.uniride.data.datasource.remote.dto.PassengerRequestRequestDto
+import com.campusmov.uniride.data.datasource.remote.dto.PassengerRequestResponseDto
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface PassengerRequestService {
     @POST("/passenger-requests")
     suspend fun savePassengerRequest(
         @Body passengerRequestRequestDto: PassengerRequestRequestDto
     )
+
+    @GET("/passenger-requests")
+    suspend fun getAllPassengerRequestsByPassengerId(
+        @Query("passengerId") passengerId: String
+    ): List<PassengerRequestResponseDto>
 }
