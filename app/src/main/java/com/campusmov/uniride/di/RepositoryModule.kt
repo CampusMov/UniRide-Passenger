4e1e6e83d06ccfb521ede282f7a518451ae4838d
elmer-riva <elmeraugustoriva@gmail.com>
2025-06-18 19:28:47 -0500

    feat(injection): add provider to inject parameters to InTripCommunicationRepository

--- app/src/main/java/com/campusmov/uniride/di/RepositoryModule.kt
+++ app/src/main/java/com/campusmov/uniride/di/RepositoryModule.kt
@@ -4,16 +4,19 @@ import com.campusmov.uniride.data.datasource.local.dao.UserDao
 import com.campusmov.uniride.data.datasource.location.LocationDataSource
 import com.campusmov.uniride.data.datasource.remote.service.AuthService
 import com.campusmov.uniride.data.datasource.remote.service.CarpoolService
+import com.campusmov.uniride.data.datasource.remote.service.InTripCommunicationService
 import com.campusmov.uniride.data.datasource.remote.service.ProfileClassScheduleService
 import com.campusmov.uniride.data.datasource.remote.service.ProfileService
 import com.campusmov.uniride.data.repository.auth.AuthRepositoryImpl
 import com.campusmov.uniride.data.repository.auth.UserRepositoryImpl
+import com.campusmov.uniride.data.repository.intripcommunication.InTripCommunicationRepositoryImpl
 import com.campusmov.uniride.data.repository.location.LocationRepositoryImpl
 import com.campusmov.uniride.data.repository.profile.ProfileClassScheduleRepositoryImpl
 import com.campusmov.uniride.data.repository.profile.ProfileRepositoryImpl
 import com.campusmov.uniride.data.repository.routingmatching.CarpoolRepositoryImpl
 import com.campusmov.uniride.domain.auth.repository.AuthRepository
 import com.campusmov.uniride.domain.auth.repository.UserRepository
+import com.campusmov.uniride.domain.intripcommunication.repository.InTripCommunicationRepository
 import com.campusmov.uniride.domain.location.repository.LocationRepository
 import com.campusmov.uniride.domain.profile.repository.ProfileClassScheduleRepository
 import com.campusmov.uniride.domain.profile.repository.ProfileRepository
@@ -44,4 +47,8 @@ object RepositoryModule {
 
     @Provides
     fun provideCarpoolRepository(carpoolService: CarpoolService): CarpoolRepository = CarpoolRepositoryImpl(carpoolService)
+
+
+    @Provides
+    fun provideInTripCommunicationRepository(inTripCommunicationService: InTripCommunicationService): InTripCommunicationRepository = InTripCommunicationRepositoryImpl(inTripCommunicationService)
 }
