310acd95b5de361c99b65208df8cf1f1e2b00229
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-01 12:37:30 -0500

    chore: remove unnecessary route

--- app/src/main/java/com/campusmov/uniride/presentation/navigation/screen/routingmatching/RoutingMatchingScreen.kt
+++ app/src/main/java/com/campusmov/uniride/presentation/navigation/screen/routingmatching/RoutingMatchingScreen.kt
@@ -3,5 +3,4 @@ package com.campusmov.uniride.presentation.navigation.screen.routingmatching
 sealed class RoutingMatchingScreen(val route: String) {
     object Home: RoutingMatchingScreen("/home")
     object SearchCarpool: RoutingMatchingScreen("/search_carpool")
-    object AddPickUpPoint: RoutingMatchingScreen("/search_carpool/add_pickup_point")
 }
