5f9b04949734ec71e803cf9e1214dcec8c33a2d0
Eric <erich56000@gmail.com>
2025-07-01 13:57:57 -0500

    feat(infraction): implement infraction function in reputation incentives repository

--- app/src/main/java/com/campusmov/uniride/data/repository/reputation/ReputationIncentivesRepositoryImpl.kt
+++ app/src/main/java/com/campusmov/uniride/data/repository/reputation/ReputationIncentivesRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.campusmov.uniride.data.repository.reputation
 
 import com.campusmov.uniride.data.datasource.remote.service.ReputationIncentivesService
+import com.campusmov.uniride.domain.reputation.model.Infraction
 import com.campusmov.uniride.domain.reputation.model.Valoration
 import com.campusmov.uniride.domain.reputation.repository.ReputationIncentivesRepository
 import com.campusmov.uniride.domain.shared.util.Resource
@@ -23,6 +24,24 @@ class ReputationIncentivesRepositoryImpl(private val reputationIncentivesService
             }
         } catch (e: Exception) {
             Resource.Failure("Exception occurred while fetching valorations: ${e.message ?: "Unknown error"}")
+        }
+    }
+
+    override suspend fun getInfractionsOfUser(userId: String): Resource<List<Infraction>> = withContext(Dispatchers.IO){
+        try {
+            val response = reputationIncentivesService.getAllPenaltiesByUserId(userId)
+            if (response.isSuccessful) {
+                val infractions = response.body() ?: emptyList()
+                if (infractions.isEmpty()) {
+                    Resource.Failure("No infractions found for user ID: $userId")
+                } else {
+                    Resource.Success(infractions.map { it.toDomain() })
+                }
+            } else {
+                Resource.Failure("Error fetching infractions: ${response.errorBody()?.string()}")
+            }
+        } catch (e: Exception) {
+            Resource.Failure("Exception occurred while fetching infractions: ${e.message ?: "Unknown error"}")
         }
     }
 }
