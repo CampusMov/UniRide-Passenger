56c16d6df8b17c5c54fb3a92f828d5a168a79d64
Eric <erich56000@gmail.com>
2025-07-03 20:46:19 -0500

    feat(carpool-completed): implement crate valoration use case

--- app/src/main/java/com/campusmov/uniride/domain/reputation/usecases/ReputationIncentivesUseCase.kt
+++ app/src/main/java/com/campusmov/uniride/domain/reputation/usecases/ReputationIncentivesUseCase.kt
@@ -2,5 +2,6 @@ package com.campusmov.uniride.domain.reputation.usecases
 
 data class ReputationIncentivesUseCase(
     val getValorationsOfUser: ValorationUseCase,
-    val getInfractionsOfUser: InfractionUseCase
+    val getInfractionsOfUser: InfractionUseCase,
+    val createValoration: ValorationUseCase
 )
