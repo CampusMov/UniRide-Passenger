d1c96907cdbf5890c3dd95669726e6ccb2d2b727
elmer-riva <elmeraugustoriva@gmail.com>
2025-07-10 01:49:44 -0500

    feat(navigation): use SplashViewModel to dynamically set start destination in RootNavGraph

--- app/src/main/java/com/campusmov/uniride/presentation/navigation/graph/root/RootNavGraph.kt
+++ app/src/main/java/com/campusmov/uniride/presentation/navigation/graph/root/RootNavGraph.kt
@@ -3,28 +3,35 @@ package com.campusmov.uniride.presentation.navigation.graph.root
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.campusmov.uniride.presentation.navigation.Graph
 import com.campusmov.uniride.presentation.navigation.graph.analytic.AnalyticsNavGraph
 import com.campusmov.uniride.presentation.navigation.graph.auth.AuthNavGraph
-import com.campusmov.uniride.presentation.navigation.graph.home.HomeNavGraph
 import com.campusmov.uniride.presentation.navigation.graph.profile.ProfileNavGraph
 import com.campusmov.uniride.presentation.navigation.graph.reputation.ReputationNavGraph
 import com.campusmov.uniride.presentation.navigation.graph.routingmatching.RoutingMatchingNavGraph
+import com.campusmov.uniride.presentation.views.SplashViewModel
+import androidx.compose.runtime.getValue
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun RootNavGraph(navHostController: NavHostController) {
-    NavHost(
-        navController = navHostController,
-        route = Graph.ROOT,
-        startDestination = Graph.AUTH
-    ) {
-        AuthNavGraph(navHostController)
-        ProfileNavGraph(navHostController)
-        RoutingMatchingNavGraph(navHostController)
-        AnalyticsNavGraph(navHostController)
-        ReputationNavGraph(navHostController)
+    val viewModel: SplashViewModel = hiltViewModel()
+    val startDestination by viewModel.startDestination.collectAsState()
+    if (startDestination != null) {
+        NavHost(
+            navController = navHostController,
+            route = Graph.ROOT,
+            startDestination = startDestination!!
+        ) {
+            AuthNavGraph(navHostController)
+            ProfileNavGraph(navHostController)
+            RoutingMatchingNavGraph(navHostController)
+            AnalyticsNavGraph(navHostController)
+            ReputationNavGraph(navHostController)
+        }
     }
 }
