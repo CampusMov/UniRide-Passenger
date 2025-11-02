04de0d12d82475a20c66d24fde65b619cbd7d5f4
Salim Ramirez <salimramirezm@gmail.com>
2025-11-02 18:06:00 -0500

    chore: add project name comment in MainActivity.kt
    Added a comment indicating the project name.

--- app/src/main/java/com/campusmov/uniride/MainActivity.kt
+++ app/src/main/java/com/campusmov/uniride/MainActivity.kt
@@ -14,6 +14,7 @@ import com.campusmov.uniride.ui.theme.UniRideTheme
 import com.google.android.libraries.places.api.Places
 import dagger.hilt.android.AndroidEntryPoint
 
+// CampusMov - UniRide
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private lateinit var navHostController: NavHostController
